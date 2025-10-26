62ef0eb364fc41b4cfbf73974a713f769d7eabf3
Mohamed Mansour El Sabbagh <61357277+MOHAMEDMANSOUR20@users.noreply.github.com>
2025-10-26 15:32:05 +0300

    Add files via upload

---
+++ lab5/lab_5/Debug/src/system_stm32f4xx.d
@@ -0,0 +1,19 @@
+src/system_stm32f4xx.o: ../src/system_stm32f4xx.c \
+ D:/Mohamed/Third_Year/Second_Term/Digital\ Systems\ in\ Medical\ Devices/labs\ projects/lab5/lab_5/CMSIS/device/stm32f4xx.h \
+ D:/Mohamed/Third_Year/Second_Term/Digital\ Systems\ in\ Medical\ Devices/labs\ projects/lab5/lab_5/CMSIS/core/core_cm4.h \
+ D:/Mohamed/Third_Year/Second_Term/Digital\ Systems\ in\ Medical\ Devices/labs\ projects/lab5/lab_5/CMSIS/core/core_cmInstr.h \
+ D:/Mohamed/Third_Year/Second_Term/Digital\ Systems\ in\ Medical\ Devices/labs\ projects/lab5/lab_5/CMSIS/core/core_cmFunc.h \
+ D:/Mohamed/Third_Year/Second_Term/Digital\ Systems\ in\ Medical\ Devices/labs\ projects/lab5/lab_5/CMSIS/core/core_cmSimd.h \
+ D:/Mohamed/Third_Year/Second_Term/Digital\ Systems\ in\ Medical\ Devices/labs\ projects/lab5/lab_5/CMSIS/device/system_stm32f4xx.h
+
+D:/Mohamed/Third_Year/Second_Term/Digital\ Systems\ in\ Medical\ Devices/labs\ projects/lab5/lab_5/CMSIS/device/stm32f4xx.h:
+
+D:/Mohamed/Third_Year/Second_Term/Digital\ Systems\ in\ Medical\ Devices/labs\ projects/lab5/lab_5/CMSIS/core/core_cm4.h:
+
+D:/Mohamed/Third_Year/Second_Term/Digital\ Systems\ in\ Medical\ Devices/labs\ projects/lab5/lab_5/CMSIS/core/core_cmInstr.h:
+
+D:/Mohamed/Third_Year/Second_Term/Digital\ Systems\ in\ Medical\ Devices/labs\ projects/lab5/lab_5/CMSIS/core/core_cmFunc.h:
+
+D:/Mohamed/Third_Year/Second_Term/Digital\ Systems\ in\ Medical\ Devices/labs\ projects/lab5/lab_5/CMSIS/core/core_cmSimd.h:
+
+D:/Mohamed/Third_Year/Second_Term/Digital\ Systems\ in\ Medical\ Devices/labs\ projects/lab5/lab_5/CMSIS/device/system_stm32f4xx.h:
---
+++ lab5/lab_5/src/main.c
@@ -0,0 +1,29 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+#include "GPIO.h"
+#include "Keypad.h"
+#include "stm32f4xx.h"
+
+
+
+int main(void)
+{
+	/* Initializing Seven Segment Pins: */
+	GPIO_Init (GPIOB, 0, 1, 1);
+	GPIO_Init (GPIOB, 1, 1, 1);
+	GPIO_Init (GPIOB, 2, 1, 1);
+	GPIO_Init (GPIOB, 3, 1, 1);
+
+	/* Initializing Keypad Pins: */
+	Keypad_init();
+
+	/* Infinite Loop: */
+	while(1) Keypad_manage();
+}
